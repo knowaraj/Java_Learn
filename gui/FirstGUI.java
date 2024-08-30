583a95f08b0caa7a2a92538f4831cab73f865303
Araj Dulal <sendmailtoaraj.com>
2024-08-30 12:00:43 +0545

    First Gui Prog

---
+++ gui/FirstGUI.java
@@ -0,0 +1,26 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FirstGUI extends JFrame{
+
+    FirstGUI(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("My title");
+        setLayout(new FlowLayout());
+        ImageIcon icon = new ImageIcon("test.png");
+        JLabel label1 = new JLabel("First");
+        JLabel label2 = new JLabel("Second");
+        JLabel label3 = new JLabel(icon);
+        add(label1);
+        add(label2);
+        add(label3);
+        setSize(400, 400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FirstGUI();
+    }
+}
