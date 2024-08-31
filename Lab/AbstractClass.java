5173d8a4238290e99de6cf9c9560402ee96ede25
Araj Dulal <sendmailtoaraj.com>
2024-08-31 22:00:30 +0545

    Dynamic Method dispatch

---
+++ Lab/AbstractClass.java
@@ -0,0 +1,31 @@
+package Lab;
+
+abstract class Animal{
+    public abstract void eat ();
+    public void sleep()
+    {
+        System.out.println("Animal sleep");
+    }
+}
+class Dog extends  Animal{
+    public void eat ()
+    {
+        System.out.println("Dog eats  ");
+    }
+}
+class Elephant extends  Animal{
+    public void eat ()
+    {
+        System.out.println("Elephant eats  ");
+    }
+}
+public class AbstractClass{
+    public static void main (String [] args){
+        Animal obj= new Elephant();
+        obj.eat();
+        obj= new Dog();
+        obj.eat();
+
+    }
+
+}
